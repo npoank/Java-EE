cba91fd3c07286ed7b1ff7a17c82c78ae824972c
Kulbicki Andrey <kubl-ml@mail.ru>
2022-11-22 10:24:30 +0300

    JSP with JAVA

---
+++ src/main/java/FirstServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1> Hello World! </h1>");
+        pw.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/FirstJSP.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Andrew
+  Date: 22.11.2022
+  Time: 10:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>FirstJSP</title>
+</head>
+<body>
+    <h1>Testing JSP</h1>
+
+    <p>
+        <% java.util.Date now = new java.util.Date();
+        String someString = "Current time " + now;
+        %>
+    <%= someString%>
+    </p>
+
+
+</body>
+</html>
